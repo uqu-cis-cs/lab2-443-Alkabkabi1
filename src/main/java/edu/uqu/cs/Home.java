47716c814cea133ec1fb131c8bd39627f9f574e4
norah <127658303+Alkabkabi1@users.noreply.github.com>
2023-04-08 18:34:39 +0000

    a lab2e

--- src/main/java/edu/uqu/cs/Home.java
+++ src/main/java/edu/uqu/cs/Home.java
@@ -14,27 +14,34 @@ public class Home{
 
 		// Create an object of type Garage 
                 //HERE
-
+                Garage g=new Garage();
 
 	        // Add "Toyota Corolla Cross" in the garage. Hint: use method addCar () of class Garage
                 //HERE
+               g.addCar ("Toyota Corolla Cross");
+
 
                 //List all the cars in the garage. Hint: use method listCars () of class Garage
                 //HERE
+               g.listCars ();
 
                 // Place "Ford Fusion" in the garage. Hint: use method addCar () of class Garage
                 //HERE
+                g.addCar("Ford Fusion");
 
  	        // List all the cars in the garage once again. Hint: use method listCars () of class Garage
                 //HERE
+                g.listCars ();
 
 
                // Move out "Ford Fusion" from the garage. Hint: use method moveOut () of class Garage
                //HERE
+               g.moveOut("Ford Fusion");
 
 
                // List all the cars in the garage. Hint: use method listCars () of class Garage
                //HERE
+               g.listCars ();
 
         }
 
